5ec2812c98b66055feda52f43b244512c625443c
Seanpr <seanpr96@gmail.com>
2019-08-16 10:27:36 -0700

    Initial commit of project with basic brick prefab

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using Interfaces;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Brick : MonoBehaviour, IDamageable
+{
+    public int health;
+    public Sprite[] images;
+
+    private int _maxHealth;
+
+    private SpriteRenderer _renderer;
+
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+
+        if (images == null || images.Length < health)
+        {
+            throw new InvalidDataException("Brick component must have images set for all hp values");
+        }
+
+        _maxHealth = health;
+        UpdateSprite();
+    }
+
+    /// <inheritdoc />
+    public bool TakeHit(int damage)
+    {
+        if ((health -= damage) <= 0)
+        {
+            Destroy(gameObject);
+            return true;
+        }
+
+        UpdateSprite();
+        return false;
+    }
+
+    public void SetColor(Color c)
+    {
+        _renderer.color = c;
+    }
+
+    private void UpdateSprite()
+    {
+        int healthLost = _maxHealth - health;
+
+        _renderer.sprite = images[healthLost];
+    }
+}
---
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -0,0 +1,12 @@
+﻿namespace Interfaces
+{
+    public interface IDamageable
+    {
+        /// <summary>
+        /// Causes an object to take damage equivalent to the passed in amount
+        /// </summary>
+        /// <param name="damage">The damage to take</param>
+        /// <returns>A bool indicating if the damage was fatal</returns>
+        bool TakeHit(int damage);
+    }
+}
